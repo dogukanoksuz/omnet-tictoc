fe28f8a14c12cc3a5dc5d29342d46c071c71f052
Doğukan Öksüz <me@dogukan.dev>
2021-05-23 12:25:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# omnet-tictoc
+OMNeT++ 2-node Tictoc Simulation
+
+#### made for computer networks lab on school
---
+++ out/clang-release/txc1.o.d
@@ -0,0 +1,4 @@
+out/clang-release//txc1.o: txc1.cc \
+  C:/Users/divergent/Desktop/omnetpp-5.6.2/include\omnetpp.h
+
+C:/Users/divergent/Desktop/omnetpp-5.6.2/include\omnetpp.h:
